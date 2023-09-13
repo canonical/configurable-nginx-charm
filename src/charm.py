823bfe7b261f38de4539318706a5a9393249cb1a
Omar Selo <omar.selo@canonical.com>
2023-09-13 10:11:06 +0000

    Pass nginx config

--- src/charm.py
+++ src/charm.py
@@ -21,6 +21,7 @@ class ConfigurableNginxCharm(ops.CharmBase):
         self._nginx_container = self.unit.get_container("nginx")
 
         self.framework.observe(self.on.nginx_pebble_ready, self._on_nginx_pebble_ready)
+        self.framework.observe(self.on.config_changed, self._on_config_changed)
 
         self._setup_ingress()
 
@@ -32,16 +33,37 @@ class ConfigurableNginxCharm(ops.CharmBase):
             service_port=80,
         )
 
+    def _on_config_changed(self, event: ops.ConfigChangedEvent):
+        """Handle config-changed event."""
+        if not self._nginx_container.can_connect():
+            event.defer()
+            return
+
+        nginx_config = self.config["nginx_config"]
+
+        if not nginx_config:
+            self.unit.status = ops.WaitingStatus("waiting for nginx config")
+            return
+
+        self._nginx_container.push("/etc/nginx/nginx.conf", nginx_config, make_dirs=True)
+
+        process = self._nginx_container.exec(["nginx", "-s", "reload"])
+        try:
+            process.wait_output()
+            self.unit.status = ops.ActiveStatus()
+        except ops.pebble.ExecError as e:
+            logger.error(e.stderr)
+            self.unit.status = ops.BlockedStatus("nginx reload failed (invalid config?)")
+
     def _on_nginx_pebble_ready(self, event: ops.PebbleReadyEvent):
         """Handle Nginx pebble-ready event."""
-        if self._nginx_container.can_connect():
-            self._nginx_container.add_layer(
-                self._nginx_service_name, self._nginx_layer, combine=True
-            )
-            self._nginx_container.replan()
-            self.unit.status = ops.ActiveStatus()
-        else:
+        if not self._nginx_container.can_connect():
             event.defer()
+            return
+
+        self._nginx_container.add_layer(self._nginx_service_name, self._nginx_layer, combine=True)
+        self._nginx_container.replan()
+        self.unit.status = ops.ActiveStatus()
 
     @property
     def _nginx_layer(self) -> ops.pebble.Layer:
